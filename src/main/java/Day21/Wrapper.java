b6fa78a21f6d85de61deab7e7d411a3c222241cc
dihia-bnb <dihiabounab@gmail.com>
2024-04-12 15:55:45 -0500

    Second push

--- src/main/java/Day21/Wrapper.java
+++ src/main/java/Day21/Wrapper.java
@@ -1,5 +1,7 @@
 package Day21;
 
+import java.util.ArrayList;
+
 public class Wrapper {
 
 	// What is wrapper class
@@ -9,28 +11,82 @@ public class Wrapper {
 	// What is parsing
 
 	public static void main(String[] args) {
-		
-		//int - Integer
-		//long - Long
-		//short - Short
-		//boolean - Boolean
-		//double - Double
-		//char - Character
-		//float - Float
-		//byte - Byte
-		
-		//Wrapper classes are pre-defined classes which can contain primitive data type.
-		//They wrap primitive data type to Wrapper Class Object
-		
+
+		// int - Integer
+		// long - Long
+		// short - Short
+		// boolean - Boolean
+		// double - Double
+		// char - Character
+		// float - Float
+		// byte - Byte
+
+		// Wrapper classes are pre-defined classes which can contain primitive data
+		// type.
+		// They wrap primitive data type to Wrapper Class Object
+
 		int a = 10;
 		System.out.println("The  primitive data type value is : " + a);
-		
+
 		Integer b = 10;
 		System.out.println("The Wrapper class value is : " + b);
 		b.intValue();
-		
-		
+
+		// When we use Collection framework
+		// to convert the primitive data types into Objects and vice versa
+
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		// ArrayList<int> array = new ArrayList<Integer>(); it not can be possible
+
+		boxingExample();
+		autoboxingExample();
+		unboxingExample();
+		autoUnboxingExample();
+	}
+	// The parent of the below classes is Number Class
+	// Integer
+	// Long
+	// Short
+	// Double
+	// Float
+	// Byte
+
+	// The parent class of the below mentioned classes is Object Class
+	// Boolean
+	// Character
+
+	// What is Boxing?
+	// Conversion of primitive data type to Wrapper Class Object
+
+	// What is Unboxing?
+	// Conversion of Wrapper Class object into primitive data type
+
+	// Homework - try boxing, unboxing, autoboxing and autounboxing for other
+	// Wrapper Class Objects and data types
+
+	public static void boxingExample() {
+		int a = 100; // a is primitive data type
+		Integer b = Integer.valueOf(a); // b is Wrapper class Object
+		System.out.println("The wrapper class object is : " + b);
+	}
+
+	public static void autoboxingExample() {
+		int a = 100;
+		Integer b = a;
+		System.out.println(b);
 
 	}
 
+	public static void unboxingExample() {
+		Integer result = 100;
+		int newResult = result.intValue();
+		System.out.println("The primitive data type is : " + newResult);
+	}
+
+	public static void autoUnboxingExample() {
+		Integer result = 100;
+		int newResult = result;
+		System.out.println("The primitive data type is : " + newResult);
+
+	}
 }
--- src/main/java/Day21/Wrapping_Double.java
+++ src/main/java/Day21/Wrapping_Double.java
@@ -3,7 +3,45 @@ package Day21;
 public class Wrapping_Double {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+	}
+
+	public static void stringToInteger() {
+
+		String price1 = "400";
+		String price2 = "250";
+		System.out.println("Total price of the commodies is : " + (price1 + price2));
+
+		int p1 = Integer.parseInt(price1);
+		int p2 = Integer.parseInt(price2);
+		System.out.println("Total price of the commodies is : " + (p1 + p2));
+
+	}
+
+	public static void stringToDouble() {
+
+		String price1 = "1.11";
+		String price2 = "2.5";
+		System.out.println("Total price of the commodies is : " + (price1 + price2));
+
+		double p1 = Double.parseDouble(price1);
+		double p2 = Double.parseDouble(price2);
+		System.out.println("Total price of the commodies is : " + (p1 + p2));
+
+	}
+	
+	public static void stringToBoolean() {
+		String result1 = "true";
+		String result2 = "false";
+		
+		boolean r1 = Boolean.parseBoolean(result1);
+		boolean r2 = Boolean.parseBoolean(result2);
+		
+		System.out.println(r1 == r2);
+
+	}
+
+	public static void stringToLong() {
 
 	}
 
